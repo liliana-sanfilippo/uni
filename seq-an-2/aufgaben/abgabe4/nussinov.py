2622761c6b5d95a3561f9250aaf41c56aeaa7f10
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2026-06-26 17:42:32 +0200

    fortschritt

--- seq-an-2/aufgaben/abgabe4/nussinov.py
+++ seq-an-2/aufgaben/abgabe4/nussinov.py
@@ -23,8 +23,8 @@ DEFAULT_SCORE = {
 DEFAULT_SCORE[("G", "C")] = 1
 DEFAULT_SCORE[("C", "G")] = 1
 
-DEFAULT_SCORE[("A", "U")] = 0
-DEFAULT_SCORE[("U", "A")] = 0
+DEFAULT_SCORE[("A", "U")] = 1
+DEFAULT_SCORE[("U", "A")] = 1
 
 DEFAULT_SCORE[("G", "U")] = 1
 DEFAULT_SCORE[("U", "G")] = 1
@@ -34,59 +34,63 @@ DEFAULT_SCORE[("U", "G")] = 1
 def nussinov_matrix(rna: str, delta : float = 1.0, score_scheme : Dict[(Tuple[str,str],float)] = DEFAULT_SCORE) -> float:
     # TODO Implement the Nussinov algorithm
     result: Dict[(Tuple[int,int],float)] = {}
-    n = len(rna)
+    n = len(rna)+1
+
+    rna = "X" + rna
 
     # mit Nullen initialisieren, damit nichts leer ist und meckert
-    for i in range(0, n):
-        for j in range(0, n):
+    for i in range(1, n):
+        for j in range(1, n):
             result[(i, j)] = 0
 
     # statt for i in range(2, len(str)) um eines verschieben, wegen 0 Start
-    for i in range(1, n):
+    for i in range(2, n):
         #print((i, i-1))
         result[(i, i-1)] = 0
 
-    for d in range(0, int(delta)):
-        for i in range(0, n):
-            #print((i, i+d))
-            j = i+d
-            if j-i <= delta:
-                result[(i, j)] = 0
-            else:
-                result[(i, i+d)] = max(
-                    result[(i+1,j)],
-                    result[(i,j-1)],
-                    result[(i+1,j-1)] + DEFAULT_SCORE[(rna[i], rna[j])]
-                )
+    for d in range(0,n-1):
+        for i in range(1,n-d):
+            j=i+d
+            if i == 1 and j == 4:
+                print(j - i > delta)
+            if j - i > delta:
+                #print(str(i) + ", " + str(j) + " sind " + rna[i] + " und " + rna[j])
+                pair_score = result[(i+1,j-1)] + score_scheme.get((rna[i], rna[j]), 0)
+                if pair_score == float("-inf"):
+                    pair_score = 0
 
-    for d in range(int(delta)+1, n):
-        for i in range(0, n-d):
-            j = i+d
-            if j-i <= delta:
-                result[(i, j)] = 0
-            else:
-                if j-i > delta+1:
+                if i == 1 and j == 4:
+                    print(result[(i+1,j-1)])
+                    print((i+1,j-1))
+                    print(pair_score)
+
+                maxi = {
+                    result[(i,k)] + result[(k+1,j)]
+                    for k in range(i,j)
+                }
+                if maxi:
                     split = max(
-                        result[(i,k-1)] + result[(k,j)]
-                        for k in range(i+2,j)
+                        maxi
                     )
                 else:
                     split = 0
 
-                result[(i, j)] = max(
+                #print("from result[(i+1,j)] = " + str(result[(i+1,j)]) + ", result[(i,j-1)] = " + str(result[(i,j-1)]))
+                result[(i,j)] = max(
                     result[(i+1,j)],
                     result[(i,j-1)],
-                    result[(i+1,j-1)] + DEFAULT_SCORE[(rna[i], rna[j])],
+                    pair_score,
                     split
                 )
 
+    print("-----------")
     print_matrix(result, n)
-    print(result[(0, n-1)])
-    return result[(0, n-1)]
+    print(result[(1, n-1)])
+    return result[(1, n-1)]
 
 
 def print_matrix(result, n):
-    for i in range(n):
-        for j in range(n):
+    for i in range(1,n):
+        for j in range(1,n):
             print(result[(i,j)], end="\t")
         print()
